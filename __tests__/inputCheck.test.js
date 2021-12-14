0c206234a07780500b88336dccb9dcb43cff6bae
santiago <72000936+santiagov916@users.noreply.github.com>
2021-12-14 09:49:57 -0800

    node, express, jest installed

--- __tests__/inputCheck.test.js
+++ __tests__/inputCheck.test.js
@@ -1,4 +1,4 @@
-const inputCheck = require('../utils/inputCheck');
+const inputCheck = require('../db/utils/inputCheck');
 
 test('inputCheck() returns null when all properties exist', () => {
   const obj = {name: 'alice'};
